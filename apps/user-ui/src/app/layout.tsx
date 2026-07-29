d1b2eca29c9061420fa3b3d8602f7802d3b09b0a
Vikrant-daksha <vikrantdaksha05@gmail.com>
2026-07-29 23:59:51 +0530

    v1.1.1 Timestamp: 5 Hour 28 Minutes

--- apps/user-ui/src/app/layout.tsx
+++ apps/user-ui/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/shared/widgets/header';
 import './global.css';
 import { Poppins, Roboto } from 'next/font/google'
+import Providers from './providers';
 
 export const metadata = {
   title: 'Cherrybrush',
@@ -27,8 +28,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${roboto.variable} ${poppins.variable}`}>
-        <Header />
-        {children}</body>
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
